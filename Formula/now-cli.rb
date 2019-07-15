e60dddc77f08e92bdf1e6a8575d80e57c81376aa
JounQin <admin@1stg.me>
2019-07-16 02:38:28 +0800

    build form source
    Inspired by: https://github.com/zeit/now-cli/issues/688#issuecomment-310335201

--- Formula/now-cli.rb
+++ Formula/now-cli.rb
@@ -1,15 +1,39 @@
+require "language/node"
+
 class NowCli < Formula
   desc "The command-line interface for Now"
   homepage "https://zeit.co/now"
-  url "https://github.com/zeit/now-cli/releases/download/15.8.0/now-macos.gz"
-  sha256 "350017e2010017e84a34a459b398f3bed24f0ffd556c78fe25c03c1ac64c3521"
+  url "https://github.com/zeit/now-cli/archive/15.8.0.tar.gz"
+  sha256 "a0ac758dfe87e67cbfded18d44d8eca657f4ec1767f44ee3b4380738859e836c"
+
+  depends_on "node" => :build
 
   def install
-    bin.install "now-macos"
-    mv "#{bin}/now-macos", "#{bin}/now"
+    inreplace "package.json" do |s|
+      s.gsub! /^.*"postinstall".*$/, "" # don't run postinstall
+    end
+
+    system "npm", "install", *Language::Node.local_npm_install_args
+    system "npm", "run", "build"
+
+    # Read the target node version from package.json
+    target = IO.read("package.json").match(/\"(node\d+(\.\d+){2})-[^"]+\"/)[1]
+
+    # This packages now-cli together with a patched version of node
+    pkg_args = %W[
+      --c=package.json
+      --o=now
+      --options=no-warnings
+      --targets=#{target}
+    ]
+    system "node_modules/.bin/pkg", "bin/now.js", *pkg_args
+
+    bin.install "now"
   end
 
   test do
-    system "#{bin}/now", "-v"
+    system "#{bin}/now", "init", "bash"
+    system "bash", "bash/index.sh"
+    system "rm", "-rf", "bash"
   end
 end
